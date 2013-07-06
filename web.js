c63e0f951490937589063a9585daa70c6630802f
abianc@ancitel.it <ubuntu@ip-172-31-15-156.us-west-2.compute.internal>
2013-07-06 04:50:44 +0000

    Corrections

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html').toString;
+  var buffer = fs.readFileSync('index.html').toString();
   response.send(buffer);
 });
 
